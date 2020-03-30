818ffe3cfd1ad18594edb89fcf8f07cb25c81994
sonu <sonu444.karmacharya@gmail.com>
2020-03-30 12:43:36 +0545

    complete login

--- modules/users.js
+++ modules/users.js
@@ -1,15 +1,30 @@
 var mongoose = require('mongoose')
-mongoose.connect('mmongodb://localhost:27017/student',{useNewUrlParser:true})
+mongoose.connect('mmongodb://localhost:27017/student',{useNewUrlParser:true, useCreateIndex:true})
 
 var conn = mongoose.connection
 
 var usersSchema = new mongoose.Schema({
-    fname: String,
-    lname: String,
-    email: String,
-    password: String,
+    username: {type:String,
+        required: true,
+        index:{
+            unique: true,
+        }},
+
+    email: {type:String,
+        required:true,
+        index:{
+            unique:true,
+        }},
+
+    password:{type:String,
+        required:true,
+    },
+    date:{type:Date,
+        default: Date.now
+
+    }
     
 })
-var usersModel = mongoose.model('Students',Schema)
+var usersModel = mongoose.model('users',usersSchema)
 
 module.exports = usersModel
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var multer = require('multer')
 var path = require('path')
+var jwt = require('jsonwebtoken')
 var stsModel = require('../modules/students')
+var userModel = require('../modules/users')
+var bcrypt = require('bcryptjs')
+
+if(typeof localStorage == "undefined" || localStorage == null){
+  const LocalStorage = require('node-localstorage').LocalStorage
+  localStorage = new LocalStorage("./scratch")
+}
+
 var router = express.Router();
 var student = stsModel.find({})
+var users = userModel.find({})
 
 router.use(express.static(__dirname+"./public/"))
 
@@ -18,14 +28,108 @@ var upload = multer({
   storage: Storage
 }).single('file')
 /* GET home page. */
+function checkEmail(req,res,next){
+  var email = req.body.email
+  var checkexitemail = userModel.findOne({email:email})
+  checkexitemail.exec((err,data)=>{
+    if(err) throw err
+    if(data){
+      return res.render('signup', { title: 'Students records System',msg:'Email already exist'});
+  
+    }
+    next()
+  })
+}
+function checkUsername(req,res,next){
+  var uname = req.body.uname
+  var checkexituname = userModel.findOne({username:uname})
+  checkexituname.exec((err,data)=>{
+    if(err) throw err
+    if(data){
+      return res.render('signup', { title: 'Students records System',msg:'Username already exist'});
+  
+    }
+    next()
+  })
+}
+function checkloginUser(req,res,next){
+  var userToken = localStorage.getItem('userToken')
+  try{
+    var decode = jwt.verify(userToken,'loginToken')
+  }
+  catch(err){
+    res.redirect('/')
+  }
+  next()
+}
 router.get('/', function(req, res, next) {
+ 
+    res.render('login', { title: 'Students records System',msg:''});
+  
+  }) 
+  router.post('/', function(req, res, next) {
+   
+    var Email = req.body.email
+    var password = req.body.pass
+    
+    var checkemail = userModel.findOne({email:Email})
+    checkemail.exec((err,data)=>{
+      if(err) throw err
+      var getUserId = data._id
+      var getPass = data.password
+      if(bcrypt.compareSync(password,getPass)){
+        var token = jwt.sign({useerId:getUserId},'loginToken')
+        localStorage.setItem('userToken',token)
+        // localStorage.setItem('logiuser',uname)
+        res.redirect('/stsform')
+     }
+      else{
+        res.render('login',{title:'Student recods system',msg:'Doesnot log in'})
+   
+      }      
+  }) })
+  router.get('/signup', function(req, res, next) {
+ 
+    res.render('signup', { title: 'Students records System',msg:''});
+  
+  })
+  router.post('/signup',checkUsername,checkEmail, function(req, res, next) {
+    var username = req.body.uname
+    var email = req.body.email
+    var password = req.body.pass
+    var cpass = req.body.cpass
+    if(password!=cpass){
+      res.render('signup', { title: 'Students records System',msg:'Password doesnot match'});
+  
+    }
+    else{
+
+   password = bcrypt.hashSync(password,10 )
+    var userDetail = new userModel({
+     username: username,
+      email:email,
+      password:password
+    })
+    userDetail.save(function(err,data){
+      if(err) throw err
+      res.render('login', { title: 'Students records System',msg:'Registered successfully'});
+    })
+  }
+   
+  })
+  router.get('/logout', function(req, res, next) {
+    localStorage.removeItem('userToken')
+    res.redirect('/')
+  });
+
+router.get('/stsform',checkloginUser, function(req, res, next) {
   student.exec(function(err,data){
     if(err) throw err
     res.render('index', { title: 'Students records',msg:''});
   
   }) 
 });
-router.post('/', upload,function(req, res, next) {
+router.post('/stsform', upload,function(req, res, next) {
   var imageFile = req.file.filename
 
   var stsDetail = new stsModel({
@@ -51,7 +155,7 @@ router.post('/', upload,function(req, res, next) {
  
 });
 
-router.get('/detail', function(req, res, next) {
+router.get('/detail', checkloginUser,function(req, res, next) {
   student.exec(function(err,data){
     if(err) throw err
     res.render('detail', { title: 'Students records',records:data});
